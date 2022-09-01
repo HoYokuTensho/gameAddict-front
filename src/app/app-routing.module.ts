67e6460df9b4ac0e293eead63dd2e78fbf5a586d
cMoi <cMonEmail@mail.com>
2022-09-01 16:56:55 +0200

    CDE : Ajout du composant 'jeu'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddGameComponent } from './add-game/add-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+        path: '',
+        component: HomeComponent
+  },
+  {
+         path: 'addGame',
+         component: AddGameComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddGameComponent } from './add-game/add-game.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AddGameComponent
+    AddGameComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
